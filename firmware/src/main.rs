b5964ead84f430334ab718c26d425e7a9f09ac52
Guillaume Pinot <guillaume.pinot@citodi.fr>
2021-10-18 10:22:11 +0200

    move the layout in its file

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -11,10 +11,8 @@ use hal::prelude::*;
 use hal::serial;
 use hal::usb;
 use hal::{stm32, timers};
-use keyberon::action::{k, l, m, Action, Action::*, HoldTapConfig};
 use keyberon::debounce::Debouncer;
 use keyberon::key_code::KbHidReport;
-use keyberon::key_code::KeyCode::*;
 use keyberon::layout::{Event, Layout};
 use keyberon::matrix::{Matrix, PressedKeys};
 use nb::block;
@@ -23,6 +21,8 @@ use stm32f0xx_hal as hal;
 use usb_device::bus::UsbBusAllocator;
 use usb_device::class::UsbClass as _;
 use usb_device::device::UsbDeviceState;
+
+mod layout;
 
 type UsbClass = keyberon::Class<'static, usb::UsbBusType, ()>;
 type UsbDevice = usb_device::device::UsbDevice<'static, usb::UsbBusType>;
@@ -38,83 +38,6 @@ impl<T> ResultExt<T> for Result<T, Infallible> {
         }
     }
 }
-
-const CUT: Action = m(&[LShift, Delete]);
-const COPY: Action = m(&[LCtrl, Insert]);
-const PASTE: Action = m(&[LShift, Insert]);
-const L2_ENTER: Action = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::HoldOnOtherKeyPress,
-    hold: &l(2),
-    tap: &k(Enter),
-};
-const L3_ENTER: Action = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::HoldOnOtherKeyPress,
-    hold: &l(3),
-    tap: &k(Enter),
-};
-const L1_SP: Action = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &l(1),
-    tap: &k(Space),
-};
-const CSPACE: Action = m(&[LCtrl, Space]);
-
-const SHIFT_ESC: Action = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &k(LShift),
-    tap: &k(Escape),
-};
-const CTRL_INS: Action = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &k(LCtrl),
-    tap: &k(Insert),
-};
-
-macro_rules! s {
-    ($k:ident) => {
-        m(&[LShift, $k])
-    };
-}
-macro_rules! a {
-    ($k:ident) => {
-        m(&[RAlt, $k])
-    };
-}
-
-#[rustfmt::skip]
-pub static LAYERS: keyberon::layout::Layers = &[
-    &[
-        &[k(Tab),     k(Q), k(W),  k(E),    k(R), k(T),    k(Y),     k(U),    k(I),   k(O),    k(P),     k(LBracket)],
-        &[k(RBracket),k(A), k(S),  k(D),    k(F), k(G),    k(H),     k(J),    k(K),   k(L),    k(SColon),k(Quote)   ],
-        &[k(Equal),   k(Z), k(X),  k(C),    k(V), k(B),    k(N),     k(M),    k(Comma),k(Dot), k(Slash), k(Bslash)  ],
-        &[Trans,      Trans,k(LGui),k(LAlt),L1_SP,k(LCtrl),k(RShift),L2_ENTER,k(RAlt),k(BSpace),Trans,   Trans      ],
-    ], &[
-        &[Trans,         k(Pause),k(ScrollLock),k(PScreen),Trans,    Trans,Trans,      k(BSpace),k(Delete),Trans,  Trans,   Trans],
-        &[Trans,         k(LGui), k(LAlt),      CTRL_INS,  SHIFT_ESC,Trans,k(CapsLock),k(Left),  k(Down),  k(Up),  k(Right),Trans],
-        &[k(NonUsBslash),k(Undo), CUT,          COPY,      PASTE,    Trans,Trans,      k(Home),  k(PgDown),k(PgUp),k(End),  Trans],
-        &[Trans,         Trans,   Trans,        Trans,     NoOp,     Trans,Trans,      L3_ENTER, Trans,    Trans,  Trans,   Trans],
-    ], &[
-        &[s!(Grave),s!(Kb1),s!(Kb2),s!(Kb3),s!(Kb4),s!(Kb5),s!(Kb6),s!(Kb7),s!(Kb8),s!(Kb9),s!(Kb0),s!(Minus)],
-        &[ k(Grave), k(Kb1), k(Kb2), k(Kb3), k(Kb4), k(Kb5), k(Kb6), k(Kb7), k(Kb8), k(Kb9), k(Kb0), k(Minus)],
-        &[a!(Grave),a!(Kb1),a!(Kb2),a!(Kb3),a!(Kb4),a!(Kb5),a!(Kb6),a!(Kb7),a!(Kb8),a!(Kb9),a!(Kb0),a!(Minus)],
-        &[Trans,    Trans,  Trans,  Trans,  CSPACE, Trans,  Trans,  NoOp,   Trans,  Trans,  Trans,  Trans    ],
-    ], &[
-        &[k(F1),k(F2),  k(F3),  k(F4),   k(F5),    k(F6),k(F7),k(F8),    k(F9),   k(F10), k(F11), k(F12)],
-        &[Trans,k(LGui),k(LAlt),k(LCtrl),k(LShift),Trans,Trans,k(RShift),k(RCtrl),k(LAlt),k(RGui),Trans ],
-        &[Trans,Trans,  Trans,  Trans,   Trans,    Trans,Trans,Trans,    Trans,   Trans,  Trans,  Trans ],
-        &[Trans,Trans,  Trans,  Trans,   Trans,    Trans,Trans,Trans,    Trans,   Trans,  Trans,  Trans ],
-    ],
-];
 
 #[app(device = crate::hal::pac, peripherals = true)]
 const APP: () = {
@@ -203,7 +126,7 @@ const APP: () = {
             timer,
             debouncer: Debouncer::new(PressedKeys::default(), PressedKeys::default(), 5),
             matrix: matrix.get(),
-            layout: Layout::new(LAYERS),
+            layout: Layout::new(crate::layout::LAYERS),
             transform,
             tx,
             rx,
