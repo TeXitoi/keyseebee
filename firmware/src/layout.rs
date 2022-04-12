a83553348e6e15d12653995c411780e9bb0facd2
Guillaume Pinot <guillaume.pinot@citodi.fr>
2022-04-12 20:42:39 +0200

    layout tweeks

--- firmware/src/layout.rs
+++ firmware/src/layout.rs
@@ -6,13 +6,6 @@ type Action = keyberon::action::Action<()>;
 const CUT: Action = m(&[LShift, Delete]);
 const COPY: Action = m(&[LCtrl, Insert]);
 const PASTE: Action = m(&[LShift, Insert]);
-const L2_ENTER: Action = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::HoldOnOtherKeyPress,
-    hold: &l(2),
-    tap: &k(Enter),
-};
 const L3_ENTER: Action = HoldTap {
     timeout: 200,
     tap_hold_interval: 0,
@@ -58,14 +51,14 @@ macro_rules! a {
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers<()> = &[
     &[
-        &[k(Tab),     k(Q), k(W),  k(E),    k(R), k(T),    k(Y),     k(U),    k(I),   k(O),    k(P),     k(LBracket)],
-        &[k(RBracket),k(A), k(S),  k(D),    k(F), k(G),    k(H),     k(J),    k(K),   k(L),    k(SColon),k(Quote)   ],
-        &[k(Equal),   k(Z), k(X),  k(C),    k(V), k(B),    k(N),     k(M),    k(Comma),k(Dot), k(Slash), k(Bslash)  ],
-        &[Trans,      Trans,k(LGui),k(LAlt),L1_SP,k(LCtrl),k(RShift),L2_ENTER,k(RAlt),k(BSpace),Trans,   Trans      ],
+        &[k(Tab),     k(Q), k(W),   k(E),   k(R), k(T),    k(Y),     k(U),k(I),   k(O),    k(P),     k(LBracket)],
+        &[k(RBracket),k(A), k(S),   k(D),   k(F), k(G),    k(H),     k(J),k(K),   k(L),    k(SColon),k(Quote)   ],
+        &[k(Equal),   k(Z), k(X),   k(C),   k(V), k(B),    k(N),     k(M),k(Comma),k(Dot), k(Slash), k(Bslash)  ],
+        &[Trans,      Trans,k(LGui),k(LAlt),L1_SP,k(LCtrl),k(RShift),l(2),k(RAlt),k(Enter),Trans,    Trans      ],
     ], &[
         &[Custom(()),    k(Pause),k(ScrollLock),k(PScreen),Trans,    Trans,Trans,      k(BSpace),k(Delete),Trans,  Trans,   Trans],
         &[Trans,         k(LGui), k(LAlt),      CTRL_INS,  SHIFT_ESC,Trans,k(CapsLock),k(Left),  k(Down),  k(Up),  k(Right),Trans],
-        &[k(NonUsBslash),k(Undo), CUT,          COPY,      PASTE,    Trans,Trans,      k(Home),  k(PgDown),k(PgUp),k(End),  Trans],
+        &[k(NonUsBslash),k(Undo), CUT,          COPY,      PASTE,    Trans,k(Enter),   k(Home),  k(PgDown),k(PgUp),k(End),  Trans],
         &[Trans,         Trans,   Trans,        Trans,     NoOp,     Trans,Trans,      L3_ENTER, Trans,    Trans,  Trans,   Trans],
     ], &[
         &[s!(Grave),s!(Kb1),s!(Kb2),s!(Kb3),s!(Kb4),s!(Kb5),s!(Kb6),s!(Kb7),s!(Kb8),s!(Kb9),s!(Kb0),s!(Minus)],
@@ -75,7 +68,7 @@ pub static LAYERS: keyberon::layout::Layers<()> = &[
     ], &[
         &[k(F1),k(F2),  k(F3),  k(F4),   k(F5),    k(F6),k(F7),k(F8),    k(F9),   k(F10), k(F11), k(F12)],
         &[Trans,k(LGui),k(LAlt),k(LCtrl),k(LShift),Trans,Trans,k(RShift),k(RCtrl),k(LAlt),k(RGui),Trans ],
-        &[Custom(()),Trans,  Trans,  Trans,   Trans,    Trans,Trans,Trans,    Trans,   Trans,  Trans,  Trans ],
+        &[Custom(()),Trans,Trans,Trans,  Trans,    Trans,Trans,Trans,    Trans,   Trans,  Trans,  Trans ],
         &[Trans,Trans,  Trans,  Trans,   Trans,    Trans,Trans,Trans,    Trans,   Trans,  Trans,  Trans ],
     ],
 ];
