15a15dd52397abb4aa91b63ae24a2780253cf39e
Guillaume <guillaume@pelikan-mobility.com>
2024-02-26 16:57:23 +0100

    nav for ergo-l

--- firmware/src/layout.rs
+++ firmware/src/layout.rs
@@ -4,9 +4,9 @@ use keyberon::key_code::KeyCode::*;
 type Action = keyberon::action::Action<()>;
 
 const TIMEOUT: u16 = 180;
-const CUT: Action = m(&&[LShift, Delete].as_slice());
-const COPY: Action = m(&&[LCtrl, Insert].as_slice());
-const PASTE: Action = m(&&[LShift, Insert].as_slice());
+const CUT: Action = m(&[LShift, Delete].as_slice());
+const COPY: Action = m(&[LCtrl, Insert].as_slice());
+const PASTE: Action = m(&[LShift, Insert].as_slice());
 const L1_SP: Action = HoldTap(&HoldTapAction {
     timeout: TIMEOUT,
     tap_hold_interval: 0,
@@ -14,31 +14,24 @@ const L1_SP: Action = HoldTap(&HoldTapAction {
     hold: l(1),
     tap: k(Space),
 });
-const CSPACE: Action = m(&&[LCtrl, Space].as_slice());
-
-const SHIFT_ESC: Action = HoldTap(&HoldTapAction {
-    timeout: TIMEOUT,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: k(LShift),
-    tap: k(Escape),
-});
-const CTRL_INS: Action = HoldTap(&HoldTapAction {
+const CSPACE: Action = m(&[LCtrl, Space].as_slice());
+const ALTGR_SHIFT: Action = m(&[RAlt, LShift].as_slice());
+const CTRL_ESC: Action = HoldTap(&HoldTapAction {
     timeout: TIMEOUT,
     tap_hold_interval: 0,
     config: HoldTapConfig::Default,
     hold: k(LCtrl),
-    tap: k(Insert),
+    tap: k(Escape),
 });
 
 macro_rules! s {
     ($k:ident) => {
-        m(&&[LShift, $k].as_slice())
+        m(&[LShift, $k].as_slice())
     };
 }
 macro_rules! a {
     ($k:ident) => {
-        m(&&[RAlt, $k].as_slice())
+        m(&[RAlt, $k].as_slice())
     };
 }
 
@@ -48,17 +41,17 @@ pub static LAYERS: keyberon::layout::Layers<12, 4, 4, ()> = keyberon::layout::la
         [Tab Q  W    E     R     T     Y    U  I   O P '[' ],
         [']' A  S    D     F     G     H    J  K   L ; '\''],
         [ =  Z  X    C     V     B     N    M  ,   . / '\\'],
-        [ n  n LGui LAlt{L1_SP}LCtrl RShift(2)RAlt n n  n  ],
+        [ n  n LGui LAlt{L1_SP}LCtrl RShift(2)RAlt {ALTGR_SHIFT} n n],
     }{
-        [{Custom(())}Pause ScrollLock PScreen       t     t    t    BSpace Delete Space Tab t],
-        [t           LGui     LAlt   {CTRL_INS}{SHIFT_ESC}t CapsLock Left   Down   Up Right t],
-        [NonUsBslash Undo    {CUT}     {COPY}    {PASTE}  t  Enter   Home  PgDown PgUp  End t],
-        [n             n       t         t          n     t    t     (3)      t    t     n  n],
+        [{Custom(())}Pause CapsLock ScrollLock PScreen n   n  BSpace Delete Space Tab n],
+        [n           LGui     LAlt   {CTRL_ESC}LShift  n   n   Left  Down   Up  Right n],
+        [NonUsBslash Undo    {CUT}     {COPY}  {PASTE} n Enter Home PgDown PgUp  End  n],
+        [n             n       t         t        n    t   t    (3)     t    t    n   n],
     }{
         [{s!(Grave)}{s!(Kb1)}{s!(Kb2)}{s!(Kb3)}{s!(Kb4)}{s!(Kb5)}{s!(Kb6)}{s!(Kb7)}{s!(Kb8)}{s!(Kb9)}{s!(Kb0)}{s!(Minus)}],
         [{ k(Grave)}{ k(Kb1)}{ k(Kb2)}{ k(Kb3)}{ k(Kb4)}{ k(Kb5)}{ k(Kb6)}{ k(Kb7)}{ k(Kb8)}{ k(Kb9)}{ k(Kb0)}{ k(Minus)}],
         [{a!(Grave)}{a!(Kb1)}{a!(Kb2)}{a!(Kb3)}{a!(Kb4)}{a!(Kb5)}{a!(Kb6)}{a!(Kb7)}{a!(Kb8)}{a!(Kb9)}{a!(Kb0)}{a!(Minus)}],
-        [n n t t {CSPACE} t t n t t n n],
+        [t t t t {CSPACE} t t n t t t t],
     }{
         [F1           F2   F3    F4    F5  F6 F7  F8     F9  F10  F11 F12],
         [t           LGui LAlt LCtrl LShift t t RShift RCtrl LAlt RGui  t],
